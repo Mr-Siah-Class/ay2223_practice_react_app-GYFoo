57419cf1c72db083e6580092d5fad840a6a508e8
Foo Guo Yi <FGY30.21@ichat.sp.edu.sg>
2022-07-07 15:14:02 +0800

    added component files #2

---
+++ react-app-example/src/components/FooterBar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class FooterBar extends React.Component {
+    constructor() {
+        super()
+    }
+
+    render()
+    {
+        return (
+            <div id='Footer'>
+                <p>Realization Lion &copy; 2022. All Rights Reserved.</p>
+            </div>
+        );
+    }
+}
+
+export default FooterBar;
